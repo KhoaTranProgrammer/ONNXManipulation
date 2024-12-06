d50799d78f62167be6d2cbd0ad07c84c131a9597
KhoaTranProgrammer <khoatranprogrammer@gmail.com>
2024-12-06 23:32:36 +0700

    Support Add operator

--- ONMAOperators.py
+++ ONMAOperators.py
@@ -15,7 +15,9 @@ default_input = \
                      [ 0.71035045, -1.6637981 ,  1.0044696 ]]).astype(np.float32),
     "Acos": np.array([[ 0.06329948, 0.0832994 ,  0.37930292],
                       [ 0.71035045, 0.6637981 ,  0.0044696 ]]).astype(np.float32),
-    "Acosh": np.array([[ 10, np.e, 1]]).astype(np.float32)
+    "Acosh": np.array([[ 10, np.e, 1]]).astype(np.float32),
+    "Add": np.array([[ 0.06329948, -1.0832994 ,  0.37930292],
+                     [ 0.71035045, -1.6637981 ,  1.0044696 ]]).astype(np.float32),
 }
 
 def ONMARandomInput(dimensions, datatype=onnx.TensorProto.FLOAT):
@@ -33,7 +35,6 @@ def Operator_1_Input_1_Output(operator_name, graph_name, inputs=["X"], outputs=[
         if input_data == None:
             x = default_input[operator_name]
             infer_input = {inputs[0]: x}
-            print(x.shape)
             input = onma_node.ONMACreateInput(inputs[0], datatype, x.shape)
             output = onma_node.ONMACreateInput(outputs[0], datatype, x.shape)
     except:
@@ -55,6 +56,40 @@ def Operator_1_Input_1_Output(operator_name, graph_name, inputs=["X"], outputs=[
 
     onma_model.ONMAInference(infer_input)
 
+def Operator_2_Inputs_1_Output(operator_name, graph_name, inputs=["X1", "X2"], outputs=["Y"], datatype=onnx.TensorProto.FLOAT, input_data1=None, input_data2=None):
+    onma_node = ONMANode()
+    onma_node.ONMAMakeNode(operator_name, inputs, outputs)
+
+    try:
+        if input_data1 == None or input_data2 == None:
+            x = default_input[operator_name]
+            infer_input = {inputs[0]: x, inputs[1]: x}
+            input1 = onma_node.ONMACreateInput(inputs[0], datatype, x.shape)
+            input2 = onma_node.ONMACreateInput(inputs[1], datatype, x.shape)
+            output = onma_node.ONMACreateInput(outputs[0], datatype, x.shape)
+    except:
+        pass
+
+    try:
+        if input_data1.all() and input_data2.all():
+            infer_input = {inputs[0]: input_data1, inputs[1]: input_data2}
+            input1 = onma_node.ONMACreateInput(inputs[0], datatype, input_data1.shape)
+            input2 = onma_node.ONMACreateInput(inputs[1], datatype, input_data2.shape)
+            output = onma_node.ONMACreateInput(outputs[0], datatype, input_data1.shape)
+    except:
+        pass
+
+    onma_graph = ONMAGraph()
+    onma_graph.ONMAMakeGraph(graph_name, [onma_node.ONMAGetNode()], [input1, input2], [output])
+
+    onma_model = ONMAModel()
+    onma_model.ONMAMakeModel(onma_graph)
+
+    onma_model.ONMAInference(infer_input)
+
 class ONMAOperators:
     def ONMAOperator_1_Input_1_Output(operator_name, graph_name, inputs=["X"], outputs=["Y"], datatype=onnx.TensorProto.FLOAT, input_data=None):
         Operator_1_Input_1_Output(operator_name, graph_name, inputs, outputs, datatype, input_data)
+
+    def ONMAOperator_2_Inputs_1_Output(operator_name, graph_name, inputs=["X1", "X2"], outputs=["Y"], datatype=onnx.TensorProto.FLOAT, input_data1=None, input_data2=None):
+        Operator_2_Inputs_1_Output(operator_name, graph_name, inputs, outputs, datatype, input_data1, input_data2)
--- main.py
+++ main.py
@@ -16,5 +16,8 @@ def main():
     if args.operator == "Abs" or args.operator == "Acos" or args.operator == "Acosh":
         ONMAOperators.ONMAOperator_1_Input_1_Output(args.operator, 'newname', inputs=["X"], outputs=["Y"], datatype=onnx.TensorProto.FLOAT)
 
+    if args.operator == "Add":
+        ONMAOperators.ONMAOperator_2_Inputs_1_Output(args.operator, 'newname', inputs=["X1", "X2"], outputs=["Y"], datatype=onnx.TensorProto.FLOAT)
+
 if main() == False:
     sys.exit(-1)
