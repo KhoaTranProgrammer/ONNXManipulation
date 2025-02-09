514635944fb5387d29df1513d1132b4516878760
KhoaTranProgrammer <khoatranprogrammer@gmail.com>
2025-02-09 14:59:20 +0700

    Parse Onnx Spec from Operators.md

--- sample.py
+++ sample.py
@@ -10,6 +10,7 @@ from onnx.backend.test.case.node import _extract_value_info
 from onnx.backend.test.case.node.affinegrid import create_theta_2d
 from onnx.backend.test.case.node.roialign import get_roi_align_input_values
 from onnx.backend.test.case.node.layernormalization import calculate_normalized_shape
+import re
 
 def expect(
     node: onnx.NodeProto,
@@ -57,14 +58,70 @@ def expect(
     model = onnx.helper.make_model_gen_version(graph, **kwargs)
 
     # Checking the produces are the expected ones.
-    sess = onnxruntime.InferenceSession(model.SerializeToString(),
-                                        providers=["CPUExecutionProvider"])
-    feeds = {name: value for name, value in zip(node.input, inputs)}
-    results = sess.run(None, feeds)
-    for expected, output in zip(outputs, results):
-        np.testing.assert_allclose(expected, output)
-        print(f'expected: {expected}')
-        print(f'output: {output}')
+    try:
+        sess = onnxruntime.InferenceSession(model.SerializeToString(),
+                                            providers=["CPUExecutionProvider"])
+        feeds = {name: value for name, value in zip(node.input, inputs)}
+        results = sess.run(None, feeds)
+        print("Finish creating InferenceSession")
+        return 1
+    except:
+        return 0
+    # for expected, output in zip(outputs, results):
+        # np.testing.assert_allclose(expected, output)
+        # print(f'expected: {expected}')
+        # print(f'output: {output}')
+
+nodes_list = ["Abs", "Acos", "Acosh", "Add", "And", "ArgMax", "ArgMin", "Asin", "Asinh", "Atan", "Atanh", "AveragePool", "BatchNormalization", "BitShift", "BitwiseAnd", "BitwiseNot", "BitwiseOr", "BitwiseXor", "Cast", "Ceil", "Col2Im", "Compress", "Concat", "ConcatFromSequence", "Constant", "ConstantOfShape", "Conv", "ConvInteger", "ConvTranspose", "Cos", "Cosh", "CumSum", "DFT", "DeformConv", "DepthToSpace", "DequantizeLinear", "Det", "Div", "Dropout", "Einsum", "Equal", "Erf", "Exp", "Expand", "EyeLike", "Flatten", "Floor", "GRU", "Gather", "GatherElements", "GatherND", "Gemm", "GlobalAveragePool", "GlobalLpPool", "GlobalMaxPool", "Greater", "GridSample", "Hardmax", "Identity", "If", "ImageDecoder", "InstanceNormalization", "IsInf", "IsNaN", "LRN", "LSTM", "Less", "Log", "Loop", "LpNormalization", "LpPool", "MatMul", "MatMulInteger", "Max", "MaxPool", "MaxRoiPool", "MaxUnpool", "Mean", "MelWeightMatrix", "Min", "Mod", "Mul", "Multinomial", "Neg", "NonMaxSuppression", "NonZero", "Not", "OneHot", "Optional", "OptionalGetElement", "OptionalHasElement", "Or", "Pad", "Pow", "QLinearConv", "QLinearMatMul", "QuantizeLinear", "RNN", "RandomNormal", "RandomNormalLike", "RandomUniform", "RandomUniformLike", "Reciprocal", "ReduceMax", "ReduceMean", "ReduceMin", "ReduceProd", "ReduceSum", "RegexFullMatch", "Reshape", "Resize", "ReverseSequence", "RoiAlign", "Round", "STFT", "Scan", "Scatter (deprecated)", "ScatterElements", "ScatterND", "SequenceAt", "SequenceConstruct", "SequenceEmpty", "SequenceErase", "SequenceInsert", "SequenceLength", "Shape", "Sigmoid", "Sign", "Sin", "Sinh", "Size", "Slice", "SpaceToDepth", "Split", "SplitToSequence", "Sqrt", "Squeeze", "StringConcat", "StringNormalizer", "StringSplit", "Sub", "Sum", "Tan", "Tanh", "TfIdfVectorizer", "Tile", "TopK", "Transpose", "Trilu", "Unique", "Unsqueeze", "Upsample (deprecated)", "Where", "Xor", "AffineGrid", "Bernoulli", "BlackmanWindow", "CastLike", "Celu", "CenterCropPad", "Clip", "DynamicQuantizeLinear", "Elu", "Gelu", "GreaterOrEqual", "GroupNormalization", "HammingWindow", "HannWindow", "HardSigmoid", "HardSwish", "LayerNormalization", "LeakyRelu", "LessOrEqual", "LogSoftmax", "MeanVarianceNormalization", "Mish", "NegativeLogLikelihoodLoss", "PRelu", "Range", "ReduceL1", "ReduceL2", "ReduceLogSum", "ReduceLogSumExp", "ReduceSumSquare", "Relu", "Selu", "SequenceMap", "Shrink", "Softmax", "SoftmaxCrossEntropyLoss", "Softplus", "Softsign", "ThresholdedRelu"]
+
+nodes_spec = {
+    "Abs": {
+        "Inputs": {
+            "X": "tensor(uint8), tensor(uint16), tensor(uint32), tensor(uint64), tensor(int8), tensor(int16), tensor(int32), tensor(int64), tensor(float16), tensor(float), tensor(double), tensor(bfloat16)"
+        },
+        "Outputs": {
+            "Y": "tensor(uint8), tensor(uint16), tensor(uint32), tensor(uint64), tensor(int8), tensor(int16), tensor(int32), tensor(int64), tensor(float16), tensor(float), tensor(double), tensor(bfloat16)"
+        }
+    },
+    "Acos": {
+        "Inputs": {
+            "X": "tensor(bfloat16), tensor(float16), tensor(float), tensor(double)"
+        },
+        "Outputs": {
+            "Y": "tensor(bfloat16), tensor(float16), tensor(float), tensor(double)"
+        }
+    },
+    "Acosh": {
+        "Inputs": {
+            "X": "tensor(bfloat16), tensor(float16), tensor(float), tensor(double)"
+        },
+        "Outputs": {
+            "Y": "tensor(bfloat16), tensor(float16), tensor(float), tensor(double)"
+        }
+    },
+    "Add": {
+        "Inputs": {
+            "A": "tensor(uint8), tensor(uint16), tensor(uint32), tensor(uint64), tensor(int8), tensor(int16), tensor(int32), tensor(int64), tensor(float16), tensor(float), tensor(double), tensor(bfloat16)",
+            "B": "tensor(uint8), tensor(uint16), tensor(uint32), tensor(uint64), tensor(int8), tensor(int16), tensor(int32), tensor(int64), tensor(float16), tensor(float), tensor(double), tensor(bfloat16)"
+        },
+        "Outputs": {
+            "C": "tensor(uint8), tensor(uint16), tensor(uint32), tensor(uint64), tensor(int8), tensor(int16), tensor(int32), tensor(int64), tensor(float16), tensor(float), tensor(double), tensor(bfloat16)"
+        }
+    },
+    "AffineGrid": {
+        "Attributes": {
+            "align_corners": {"type": int, "default": 0, "optional": False}
+        },
+        "Inputs": {
+            "theta": "tensor(bfloat16), tensor(float16), tensor(float), tensor(double)",
+            "size": "tensor(int64)"
+        },
+        "Outputs": {
+            "grid": "tensor(bfloat16), tensor(float16), tensor(float), tensor(double)"
+        }
+    }
+    #"And", "ArgMax", "ArgMin", "Asin", "Asinh", "Atan", "Atanh", "AveragePool", "BatchNormalization", "BitShift", "BitwiseAnd", "BitwiseNot", "BitwiseOr", "BitwiseXor", "Cast", "Ceil", "Col2Im", "Compress", "Concat", "ConcatFromSequence", "Constant", "ConstantOfShape", "Conv", "ConvInteger", "ConvTranspose", "Cos", "Cosh", "CumSum", "DFT", "DeformConv", "DepthToSpace", "DequantizeLinear", "Det", "Div", "Dropout", "Einsum", "Equal", "Erf", "Exp", "Expand", "EyeLike", "Flatten", "Floor", "GRU", "Gather", "GatherElements", "GatherND", "Gemm", "GlobalAveragePool", "GlobalLpPool", "GlobalMaxPool", "Greater", "GridSample", "Hardmax", "Identity", "If", "ImageDecoder", "InstanceNormalization", "IsInf", "IsNaN", "LRN", "LSTM", "Less", "Log", "Loop", "LpNormalization", "LpPool", "MatMul", "MatMulInteger", "Max", "MaxPool", "MaxRoiPool", "MaxUnpool", "Mean", "MelWeightMatrix", "Min", "Mod", "Mul", "Multinomial", "Neg", "NonMaxSuppression", "NonZero", "Not", "OneHot", "Optional", "OptionalGetElement", "OptionalHasElement", "Or", "Pad", "Pow", "QLinearConv", "QLinearMatMul", "QuantizeLinear", "RNN", "RandomNormal", "RandomNormalLike", "RandomUniform", "RandomUniformLike", "Reciprocal", "ReduceMax", "ReduceMean", "ReduceMin", "ReduceProd", "ReduceSum", "RegexFullMatch", "Reshape", "Resize", "ReverseSequence", "RoiAlign", "Round", "STFT", "Scan", "Scatter (deprecated)", "ScatterElements", "ScatterND", "SequenceAt", "SequenceConstruct", "SequenceEmpty", "SequenceErase", "SequenceInsert", "SequenceLength", "Shape", "Sigmoid", "Sign", "Sin", "Sinh", "Size", "Slice", "SpaceToDepth", "Split", "SplitToSequence", "Sqrt", "Squeeze", "StringConcat", "StringNormalizer", "StringSplit", "Sub", "Sum", "Tan", "Tanh", "TfIdfVectorizer", "Tile", "TopK", "Transpose", "Trilu", "Unique", "Unsqueeze", "Upsample (deprecated)", "Where", "Xor", "AffineGrid", "Bernoulli", "BlackmanWindow", "CastLike", "Celu", "CenterCropPad", "Clip", "DynamicQuantizeLinear", "Elu", "Gelu", "GreaterOrEqual", "GroupNormalization", "HammingWindow", "HannWindow", "HardSigmoid", "HardSwish", "LayerNormalization", "LeakyRelu", "LessOrEqual", "LogSoftmax", "MeanVarianceNormalization", "Mish", "NegativeLogLikelihoodLoss", "PRelu", "Range", "ReduceL1", "ReduceL2", "ReduceLogSum", "ReduceLogSumExp", "ReduceSumSquare", "Relu", "Selu", "SequenceMap", "Shrink", "Softmax", "SoftmaxCrossEntropyLoss", "Softplus", "Softsign", "ThresholdedRelu"
+}
 
 # Element type: https://onnx.ai/onnx/intro/concepts.html#element-type
 element_type = {
@@ -75,7 +132,7 @@ element_type = {
     "onnx.TensorProto.INT16": np.int16,
     "onnx.TensorProto.INT32": np.int32,
     "onnx.TensorProto.INT64": np.int64,
-    "onnx.TensorProto.STRING": np.U7,
+    "onnx.TensorProto.STRING": np.string_,
     "onnx.TensorProto.BOOL": np.bool_,
     "onnx.TensorProto.FLOAT16": np.float16,
     "onnx.TensorProto.DOUBLE": np.float64,
@@ -96,13 +153,171 @@ element_type = {
 def createSampleData(dimentions, datatype):
     return np.random.randn(*dimentions).astype(datatype)
 
-node = onnx.helper.make_node(
-    "Abs",
-    inputs=["x"],
-    outputs=["y"],
-)
-x = np.random.randn(3, 4, 5).astype(np.float32)
-y = np.abs(x)
+def readSpecForOneNode(spec):
+    isInput = False
+    input = []
+    isOutput = False
+    output = []
+    isTypeConstraints = False
+    type = []
+    isAttributes = False
+    attributes = []
 
-print(createSampleData([1, 2, 3], element_type["onnx.TensorProto.INT8"]))
-# expect(node, inputs=[x], outputs=[y], name="test_abs")
+    node_name = ""
+    node_spec = {}
+    node_input = {}
+    node_output = {}
+    node_type = {}
+    node_attributes = {}
+
+    for line in spec:
+        result = re.findall(r"^### <a name=*>*", line) # Get start segment of node
+        if result != []:
+            node_name = (line.split(">**"))[1]
+            node_name = (node_name.split("**<"))[0]
+
+        if  "#### Inputs" in line:
+            isInput = True
+            isOutput = False
+            isTypeConstraints = False
+            isAttributes = False
+
+        if "#### Outputs" in line:
+            isInput = False
+            isOutput = True
+            isTypeConstraints = False
+            isAttributes = False
+
+        if "#### Type Constraints" in line:
+            isInput = False
+            isOutput = False
+            isTypeConstraints = True
+            isAttributes = False
+
+        if "#### Examples" in line:
+            isInput = False
+            isOutput = False
+            isTypeConstraints = False
+            isAttributes = False
+
+        if "#### Attributes" in line:
+            isInput = False
+            isOutput = False
+            isTypeConstraints = False
+            isAttributes = True
+
+        if isInput: input.append(line)
+        if isOutput: output.append(line)
+        if isTypeConstraints: type.append(line)
+        if isAttributes: attributes.append(line)
+    
+    for item in type:
+        if "<dt><tt>" in item: # <dt><tt>T</tt> : tensor(uint8), tensor(uint16)
+            item_sepa = item.split(" : ")
+            type_name = item_sepa[0]
+            type_name = type_name.replace("<dt><tt>", "")
+            type_name = type_name.replace("</tt>", "")
+            type_value = item_sepa[1]
+            type_value = type_value.replace("</dt>", "")
+            type_value = type_value.replace("tensor(", "")
+            type_value = type_value.replace("), ", ",")
+            type_value = type_value.replace(")\n", "")
+            node_type[type_name] = type_value
+
+    for item in input:
+        if "<dt><tt>" in item: # <dt><tt>X</tt> (differentiable) : T</dt>
+            item_sepa = item.split(" : ")
+            input_name = item_sepa[0]
+            input_name = input_name.replace("<dt><tt>", "")
+            input_name = input_name.replace("</tt> (differentiable)", "")
+            input_name = input_name.replace("</tt> (non-differentiable)", "")
+            input_value = item_sepa[1]
+            input_value = input_value.replace("</dt>\n", "")
+            node_input[input_name] = input_value
+
+    for item in output:
+        if "<dt><tt>" in item: # <dt><tt>X</tt> (differentiable) : T</dt>
+            item_sepa = item.split(" : ")
+            output_name = item_sepa[0]
+            output_name = output_name.replace("<dt><tt>", "")
+            output_name = output_name.replace("</tt> (differentiable)", "")
+            output_name = output_name.replace("</tt> (non-differentiable)", "")
+            output_value = item_sepa[1]
+            output_value = output_value.replace("</dt>\n", "")
+            node_output[output_name] = output_value
+
+    for item in attributes:
+        if "<dt><tt>" in item: # <dt><tt>axis</tt> : int (default is 0)</dt>
+            item_sepa = item.split(" : ")
+            attributes_name = item_sepa[0]
+            attributes_name = attributes_name.replace("<dt><tt>", "")
+            attributes_name = attributes_name.replace("</tt>", "")
+            attributes_value = item_sepa[1]
+            attributes_value = attributes_value.replace("</dt>\n", "")
+            node_attributes[attributes_name] = attributes_value
+
+    node_spec["Attributes"] = node_attributes
+    node_spec["Inputs"] = node_input
+    node_spec["Outputs"] = node_output
+    node_spec["Type"] = node_type
+    return node_name, node_spec
+
+def readOnnxNodeSpec():
+    node_dict = {}
+    # Open the file in read mode
+    with open('Operators.md', 'r') as file:
+        isStart = False
+        spec = []
+        # Read each line in the file
+        for line in file:
+            if "## ai.onnx.preview.training" in line and spec != []:
+                isStart = False
+                node_name, node_spec = readSpecForOneNode(spec)
+                node_dict[node_name] = node_spec
+                print(node_name)
+                print(node_spec)
+                spec = []
+
+            result = re.findall(r"^### <a name=*>*", line) # Get start segment of node
+            if result != []:
+                if "ai.onnx.preview.training" not in line:
+                    isStart = True
+                    if spec != []:
+                        node_name, node_spec = readSpecForOneNode(spec)
+                        node_dict[node_name] = node_spec
+                        print(node_name)
+                        print(node_spec)
+                        spec = []
+                else:
+                    isStart = False
+                    spec = []
+
+            if isStart == True:
+                spec.append(line)
+
+# for item in nodes_list:
+#     if item == "Add":
+#         print(f'Node: {item}')
+#         node = onnx.helper.make_node(
+#             item,
+#             inputs=["x1"],
+#             outputs=["y"],
+#         )
+#         print(f'Node: {item} is created OK')
+#         x = createSampleData([1, 2, 3], np.float32)
+#         y = x
+#         status = expect(node, inputs=[x, x], outputs=[y], name="test_abs")
+#         if status: print("Inferrence is OK")
+#         else: print("Inferrence is FAILED")
+
+        # for datatype in element_type:
+        #     try:
+        #         x = createSampleData([1, 2, 3], element_type[datatype])
+        #         y = x
+        #         status = expect(node, inputs=[x, x], outputs=[y], name="test_abs")
+        #         if status: print("Inferrence is OK")
+        #         else: print("Inferrence is FAILED")
+        #     except:
+        #         pass
+
+readOnnxNodeSpec()
