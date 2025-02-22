95f326012286409136844b8ce23925d2a652c7e6
KhoaTranProgrammer <khoatranprogrammer@gmail.com>
2025-02-22 23:10:25 +0700

    Support some attributes: axis, keepdims, select_last_index

--- sample.py
+++ sample.py
@@ -108,7 +108,10 @@ element_type = {
 attributes = {
     "direction": ["RIGHT", "LEFT"],
     "to": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
-    "kernel_shape": [[3, 3]]
+    "kernel_shape": [[3, 3]],
+    "axis": [0],
+    "keepdims": [1],
+    "select_last_index": [0]
 }
 
 input_special = {
